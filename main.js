0e25c47bfa56c09f327ad2fcbde7a92faf880d91
Denys <gerasymenkoden@gmail.com>
2021-01-28 13:51:42 -0800

    continue

--- main.js
+++ main.js
@@ -1,39 +1,76 @@
+import './style.scss';
+
 class Carousel {
     constructor(
-
+        limit = 3,
     ) {
         this.$nextBtn = document.querySelector('.next-btn');
         this.$prevBtn = document.querySelector('.prev-btn');
-        this.position = 0;
         this.container = document.querySelector('.carousel-container');
-        this.containerWidth = this.container.getClientRects()[0].width + 20;
+        this.containerWidth = this.container.getClientRects()[0].width;
+        this.limit = limit;
     }
 
-    nextSlide() {
+    changeSlide(isNext = true) {
         const currentSlide = document.querySelector('.slide.active');
-        const closest = currentSlide.nextElementSibling;
-        closest.style.transform -= this.containerWidth;
+        const closest = currentSlide[isNext ? 'nextElementSibling' : 'previousElementSibling'];
+        closest.style.transform = `translateX(0px)`;
         closest.classList.add('active');
         currentSlide.classList.remove('active');
-        this.position -= this.containerWidth;
-        currentSlide.style.transform = `translateX(${this.position}px)`;
+        currentSlide.style.transform = `translateX(${isNext ? '-' : ''}${this.containerWidth}px)`;
     }
 
-    prevSlide() {
+    removeSlides(isNext = true) {
+        const allSlides = document.querySelectorAll('.slide');
         const currentSlide = document.querySelector('.slide.active');
-        this.position += this.containerWidth;
-        currentSlide.style.transform = `translateX(${this.position}px)`;
+        const currentSlideIndex = currentSlide.getAttribute('data-slide-index');
+
+        if (isNext) {
+            if(currentSlideIndex - 2 >= 0) {
+                const forRemove = document.querySelector(`[data-slide-index='${currentSlideIndex - 2}']`);
+                let clone = forRemove.cloneNode(true);
+                forRemove.remove();
+
+
+                for (let i = 0; i < allSlides.length; i++) {
+                    allSlides[i].setAttribute('data-slide-index', i.toString())
+                }
+
+                clone.setAttribute('data-slide-index', allSlides.length);
+                clone.style.transform = `translateX(${this.containerWidth}px)`;
+
+                document.querySelector('ul').append(clone);
+            }
+        } else {
+            if(currentSlideIndex + 2 < allSlides.length) {
+                const forRemove = document.querySelector(`[data-slide-index='${currentSlideIndex + 2}']`);
+                let clone = forRemove.cloneNode(true);
+                forRemove.remove();
+
+
+                for (let i = 0; i < allSlides.length; i++) {
+                    allSlides[i].setAttribute('data-slide-index', i.toString())
+                }
+
+                clone.setAttribute('data-slide-index', allSlides.length);
+                clone.style.transform = `translateX(${this.containerWidth}px)`;
+
+                document.querySelector('ul').append(clone);
+            }
+        }
     }
 
     addEvents() {
         this.$nextBtn.addEventListener('click', e => {
             e.preventDefault()
-            this.nextSlide()
+            this.changeSlide()
+            this.removeSlides()
         })
 
         this.$prevBtn.addEventListener('click', e => {
             e.preventDefault()
-            this.prevSlide()
+            this.changeSlide(false);
+            this.removeSlides(false)
         })
     }
 
@@ -42,17 +79,43 @@ class Carousel {
         this.slides = slides;
         this.template = template;
 
-        return `
-            <div>hello</div>
-        `
+        const list = document.querySelector('ul');
+
+        for (let i = 0; i < this.limit; i++) {
+            const listItem = document.createElement('li');
+            listItem.innerText = this.slides[i];
+
+            listItem.classList.add('slide');
+            listItem.setAttribute('data-slide-index', i.toString());
+            list.append(listItem);
+
+            if (i === 0) {
+                listItem.classList.add('active');
+                listItem.style.background = 'red';
+            }
+
+            if (i === 1) {
+                listItem.style.transform += `translateX(${this.containerWidth}px)`;
+                listItem.style.background = 'green';
+            }
+
+            if (i === 2) {
+                listItem.style.transform += `translateX(-${this.containerWidth}px)`;
+                listItem.style.background = 'blue';
+            }
+        }
     }
 }
 
-const root = document.querySelector('#root');
 const carousel = new Carousel();
 
-console.log(root)
 
-carousel.render()
+const slides = [
+    'slide 1',
+    'slide 2',
+    'slide 3',
+    'slide 4',
+    'slide 5',
+];
+carousel.render(slides)
 
-// root.insertAdjacentHTML(carousel.render());
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './main.js'
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Main html',
+            template: "./index.html",
+            filename: "index.html"
+        }),
+    ],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    browsers:['ie >= 8', 'last 4 version']
+                                })
+                            ],
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+};
